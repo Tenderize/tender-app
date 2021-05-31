e223006b3cb40bfc65c13ffeca3a74a1b7339374
Nico Vergauwen <vergauwennico@gmail.com>
2021-05-31 04:40:58 +0200

    matic rinkeby

--- packages/app/src/data/stakers.tsx
+++ packages/app/src/data/stakers.tsx
@@ -25,7 +25,7 @@ type Stakers = {
         description: "Matic Network brings massive scale to Ethereum using an adapted version of Plasma with PoS based side chains. The Matic team a decentralized platform using an adapted version of Plasma framework that provides a solution for faster and extremely low-cost transactions with finality on the main chain. The system ensures liveliness using PoS checkpoints which are pushed to the Ethereum mainchain. This enables a single Matic sidechain to theoretically achieve 2 16 transactions per block, and possibly millions of transactions on multiple chains in the future.",
         stakerAddress: "",
         available: true,
-        apy: 0,
+        apy: 14.8,
         logo: "matic.svg",
         symbol: "MATIC"
       },
