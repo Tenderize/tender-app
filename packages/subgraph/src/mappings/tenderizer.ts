5d47b0d2fcd6f1c5705706c840da6ba75b44a235
Reuben Rodrigues <reuben.r@smarter.codes>
2021-07-02 03:22:01 -0700

    Subgraph: BigInt to BigDecimal

--- packages/subgraph/src/mappings/tenderizer.ts
+++ packages/subgraph/src/mappings/tenderizer.ts
@@ -22,6 +22,7 @@ import {
 export function handleDepositEvent(depositEvent: Deposit): void {
   let tenderizerAddress = depositEvent.address.toHex()
   let tenderizerId  = getTenderizerIdByTenderizerAddress(tenderizerAddress)
+  let amount = depositEvent.params.amount.toBigDecimal()
 
   // Sanity check, tenderizers would generally always be registered
   if(tenderizerId ==  ''){
@@ -31,18 +32,18 @@ export function handleDepositEvent(depositEvent: Deposit): void {
 
   // Update day data
   let day = loadOrCreateDay(depositEvent.block.timestamp.toI32(), tenderizerId)
-  day.tenderizerDepositVolume = day.tenderizerDepositVolume.plus(depositEvent.params.amount.toBigDecimal())
-  day.totalTenderizerDeposit = day.totalTenderizerDeposit.plus(depositEvent.params.amount.toBigDecimal())
+  day.tenderizerDepositVolume = day.tenderizerDepositVolume.plus(amount)
+  day.totalTenderizerDeposit = day.totalTenderizerDeposit.plus(amount)
   day.save()
 
   // Update Tenderizer deposit total
   let tenderizer = loadOrCreateTenderizer(tenderizerId)
-  tenderizer.deposits = tenderizer.deposits.plus(depositEvent.params.amount)
+  tenderizer.deposits = tenderizer.deposits.plus(amount)
   tenderizer.save()
 
   // Update User data
   let userData = loadOrCreateUserTenderizerData(depositEvent.params.from.toHex(), tenderizerId)
-  userData.deposits = userData.deposits.plus(depositEvent.params.amount)
+  userData.deposits = userData.deposits.plus(amount)
   userData.save()
 
   // Save raw event
@@ -57,6 +58,7 @@ export function handleDepositEvent(depositEvent: Deposit): void {
 export function handleWithdrawEvent(withdrawEvent: Withdraw): void {
   let tenderizerAddress = withdrawEvent.address.toHex()
   let tenderizerId  = getTenderizerIdByTenderizerAddress(tenderizerAddress)
+  let amount = withdrawEvent.params.amount.toBigDecimal()
 
   // Sanity check, tenderizers would generally always be registered
   if(tenderizerId ==  ''){
@@ -66,18 +68,18 @@ export function handleWithdrawEvent(withdrawEvent: Withdraw): void {
 
   // Update day data
   let day = loadOrCreateDay(withdrawEvent.block.timestamp.toI32(), tenderizerId)
-  day.tenderizerDepositVolume = day.tenderizerDepositVolume.minus(withdrawEvent.params.amount.toBigDecimal())
-  day.totalTenderizerDeposit = day.totalTenderizerDeposit.minus(withdrawEvent.params.amount.toBigDecimal())
+  day.tenderizerDepositVolume = day.tenderizerDepositVolume.minus(amount)
+  day.totalTenderizerDeposit = day.totalTenderizerDeposit.minus(amount)
   day.save()
 
   // Update Tenderizer deposit total
   let tenderizer = loadOrCreateTenderizer(tenderizerId)
-  tenderizer.withdrawals = tenderizer.withdrawals.plus(withdrawEvent.params.amount)
+  tenderizer.withdrawals = tenderizer.withdrawals.plus(amount)
   tenderizer.save()
 
   // Update User data
   let userData = loadOrCreateUserTenderizerData(withdrawEvent.params.from.toHex(), tenderizerId)
-  userData.withdrawals = userData.withdrawals.plus(withdrawEvent.params.amount)
+  userData.withdrawals = userData.withdrawals.plus(amount)
   userData.save()
 
   // Save raw event
@@ -92,6 +94,7 @@ export function handleWithdrawEvent(withdrawEvent: Withdraw): void {
 export function handleRewardsClaimedEvent(rewardsClaimedEvent: RewardsClaimed): void {
   let tenderizerAddress = rewardsClaimedEvent.address.toHex()
   let tenderizerId  = getTenderizerIdByTenderizerAddress(tenderizerAddress)
+  let amount = rewardsClaimedEvent.params.rewards.toBigDecimal()
 
   // Sanity check, tenderizers would generally always be registered
   if(tenderizerId ==  ''){
@@ -101,13 +104,13 @@ export function handleRewardsClaimedEvent(rewardsClaimedEvent: RewardsClaimed): 
 
   // Update day data
   let day = loadOrCreateDay(rewardsClaimedEvent.block.timestamp.toI32(), tenderizerId)
-  day.rewardsVolume = day.rewardsVolume.plus(rewardsClaimedEvent.params.rewards.toBigDecimal())
-  day.totalRewards = day.totalRewards.plus(rewardsClaimedEvent.params.rewards.toBigDecimal())
+  day.rewardsVolume = day.rewardsVolume.plus(amount)
+  day.totalRewards = day.totalRewards.plus(amount)
   day.save()
   
   // Update Tenderizer deposit total
   let tenderizer = loadOrCreateTenderizer(tenderizerId)
-  tenderizer.rewards = tenderizer.rewards.plus(rewardsClaimedEvent.params.rewards)
+  tenderizer.rewards = tenderizer.rewards.plus(amount)
   tenderizer.save()
 
   // Save raw event
@@ -131,7 +134,7 @@ export function handleProtocolFeeCollectedEvent(protocolFeeCollectedEvent: Proto
 
   // Update Tenderizer deposit total
   let tenderizer = loadOrCreateTenderizer(tenderizerId)
-  tenderizer.protocolFees = tenderizer.protocolFees.plus(protocolFeeCollectedEvent.params.amount)
+  tenderizer.protocolFees = tenderizer.protocolFees.plus(protocolFeeCollectedEvent.params.amount.toBigDecimal())
   tenderizer.save()
 
   // Save raw event
@@ -154,7 +157,7 @@ export function handleLiquidityFeeCollectedEvent(liquidityFeeCollectedEvent: Liq
 
   // Update Tenderizer deposit total
   let tenderizer = loadOrCreateTenderizer(tenderizerId)
-  tenderizer.liquidityFees = tenderizer.liquidityFees.plus(liquidityFeeCollectedEvent.params.amount)
+  tenderizer.liquidityFees = tenderizer.liquidityFees.plus(liquidityFeeCollectedEvent.params.amount.toBigDecimal())
   tenderizer.save()
 
   // Save raw event
