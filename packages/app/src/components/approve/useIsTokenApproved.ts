dfa116efb7be79544027102ca213d5b4ca44997a
Mark Vujevits <vujevits.mark@gmail.com>
2021-07-05 15:35:59 +0200

    use ApproveToken in staking section (#23)

---
+++ packages/app/src/components/approve/useIsTokenApproved.ts
@@ -0,0 +1,10 @@
+import { useEthers, useTokenAllowance } from "@usedapp/core";
+import { utils } from "ethers";
+
+export const useIsTokenApproved = (tokenAddress: string, spenderAddress: string, amount: string): boolean => {
+  const { account } = useEthers();
+  const allowance = useTokenAllowance(tokenAddress, account, spenderAddress);
+  const isTokenApproved = allowance != null && allowance.gte(amount === "" ? "0" : utils.parseEther(amount));
+
+  return isTokenApproved;
+};
