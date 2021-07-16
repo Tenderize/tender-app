90ee715ce6a02426df629c1cd8736263b889812f
Nico Vergauwen <vergauwennico@gmail.com>
2021-07-16 21:25:55 +0200

    more homepage styling

--- packages/app/src/components/featured-card/index.tsx
+++ packages/app/src/components/featured-card/index.tsx
@@ -18,12 +18,9 @@ const FeaturedCards: FC = () => {
     );
   }
   return (
-    <Box flex={true} align="end">
-      <Text>More coming soon</Text>
-      <Box flex={true} width="100%" direction="row" gap="small">
+      <Box flex={true} height="fill" width="100%" direction="row" gap="small" justify='end' align='center'>
         {cards}
       </Box>
-    </Box>
   );
 };
 
--- packages/app/src/pages/home/index.tsx
+++ packages/app/src/pages/home/index.tsx
@@ -2,23 +2,34 @@ import { FC } from "react";
 import FeaturedCards from "../../components/featured-card/";
 import HammerSteak from "../../components/hammersteak";
 import { Box, Grid, Main, Heading, Text, Button } from "grommet";
+import styled from 'styled-components'
+const MyStyledButton = styled(Button)`
+  font-weight: bold;
+  background-color: #FFFFFF;
+  color: #4E66DE;
+  border: none;
+  width: 270px;
+  height: 70px;
+  border-radius: none;
+`;
+
 
 const Home: FC = () => {
   return (
-    <Box flex fill overflow="hidden">
-      <HammerSteak/>
+    <>
+    <HammerSteak/>
+    <Box flex>
       <Grid
         fill
-        rows={["small", "medium"]}
+        rows={["medium", "flex"]}
         columns={["1/5", "4/5"]}
         gap="small"
         areas={[
           { name: "main", start: [0, 0], end: [1, 0] },
-          { name: "fill", start: [0, 1], end: [0, 1] },
           { name: "feature", start: [1, 1], end: [1, 1] },
         ]}
       >
-        <Main gridArea="main"flex justify="center" align="center">
+        <Main gridArea="main" justify="center" align="center" gap="medium">
             <Heading  color={"white"} style={{ marginTop: "1em" }}>
               Tenderize
             </Heading>
@@ -26,14 +37,14 @@ const Home: FC = () => {
               Don't just stake,
               <span style={{ color: "#F8F8F8", fontWeight: 900 }}>{" Tenderize first "}</span>
             </Text>
-            <Button primary label="Open App"></Button>
+            <MyStyledButton label="Open App" />
         </Main>
-        <Box gridArea="fill"  />
         <Box gridArea="feature" className="blur-box">
         <FeaturedCards /* provider={this.props.provider} */ />
         </Box>
       </Grid>
     </Box>
+    </>
   );
 };
 
