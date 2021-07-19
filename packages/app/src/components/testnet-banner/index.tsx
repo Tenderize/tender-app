8947e6ef67c3a99afbe3f15106296fd2171811fe
Mark Vujevits <vujevits.mark@gmail.com>
2021-07-19 02:42:47 +0200

    rework a bit the grid layout to accomodate the footer (#40)
    * rework a bit the grid layout to accomodate the footer
    
    * min height

--- packages/app/src/components/testnet-banner/index.tsx
+++ packages/app/src/components/testnet-banner/index.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 
 const TestnetBanner: FC = () => {
   return (
-    <Box background="#FFE8B9" style={{ height: 40 }} align="center" justify="around">
+    <Box background="#FFE8B9" height="40px" align="center" justify="around">
       <Text color="gray" size="small">
         Tenderize.me is currently only live on Rinkeby testnet.
       </Text>
