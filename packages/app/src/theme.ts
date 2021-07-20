5a26ab1c741b040719e61798cd053156ca489310
Nico Vergauwen <vergauwennico@gmail.com>
2021-07-20 02:38:00 +0200

    clean up grommet theme

--- packages/app/src/theme.ts
+++ packages/app/src/theme.ts
@@ -5,7 +5,7 @@ const overrideDefaultTheme: ThemeType = {
   global: {
     colors: {
       brand: "#4E66DE",
-      "light-1": "#FFFFFF",
+      text: "#FFFFFF",
     },
     elevation: {
       light: {
@@ -30,69 +30,18 @@ const overrideDefaultTheme: ThemeType = {
     },
   },
   button: {
-    default: {
-      color: "#FFFFFF",
-      border: {
-        color: "rgba(255,255,255,0)",
-        width: "1px",
-      },
-    },
     primary: {
-      background: "white",
-      color: "#4E66DE",
-      border: {
-        color: "rgba(255,255,255,0)",
-        width: "1px",
-      },
+      extend: `
+      padding: 10px 20px;
+      border-width: -1px;
+      `
     },
     secondary: {
-      background: "#4E66DE",
-      color: "#FFFFFF",
-      border: {
-        color: "rgba(255,255,255,0)",
-        width: "1px",
-      },
-    },
-    hover: {
-      default: {
-        background: "white",
-        color: "#4E66DE",
-        border: {
-          color: "4E66DE",
-          width: "1px",
-        },
-      },
-      primary: {
-        background: "#4E66DE",
-        color: "white",
-        border: {
-          width: "1px",
-          color: "white",
-        },
-      },
-      secondary: {
-        background: "white",
-        color: "#4E66DE",
-        border: {
-          color: "4E66DE",
-          width: "1px",
-        },
-      },
-    },
-    // secondary: {
-    //   extend: `
-    //   background: #4E66DE;
-    //   color: white;
-    //   border: none;
-    //   border-radius: 0px;
-    //   font-family: "IBM Plex Mono";
-    //   transition: 0.3s;
-    //   &:hover{
-    //     background: white;
-    //     color: #4E66DE;
-    //     border: 1px solid #4E66DE !important;
-    //   }`
-    // }
+      extend: `
+      padding: 10px 20px;
+      border-width: -1px;
+      `
+    }
   },
   formField: {
     border: false,
