376c06f0b78d13f9f5ab046c9b8e57d86cdd4e90
Mark Vujevits <vujevits.mark@gmail.com>
2021-08-06 18:52:38 +0200

    check if allowance is greater than 0 if amount is empty

--- packages/app/src/components/approve/useIsTokenApproved.ts
+++ packages/app/src/components/approve/useIsTokenApproved.ts
@@ -4,7 +4,7 @@ import { utils } from "ethers";
 export const useIsTokenApproved = (tokenAddress: string, spenderAddress: string, amount: string): boolean => {
   const { account } = useEthers();
   const allowance = useTokenAllowance(tokenAddress, account, spenderAddress);
-  const isTokenApproved = allowance != null && allowance.gte(amount === "" ? "0" : utils.parseEther(amount));
+  const isTokenApproved = allowance != null && allowance.gt(amount === "" ? "0" : utils.parseEther(amount));
 
   return isTokenApproved;
 };
