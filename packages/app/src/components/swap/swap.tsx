95bf9b7e267b317d4ee41806fbc17da845a3378b
Mark Vujevits <vujevits.mark@gmail.com>
2021-08-13 10:44:39 +0200

    fix swap buttons width

--- packages/app/src/components/swap/swap.tsx
+++ packages/app/src/components/swap/swap.tsx
@@ -158,7 +158,7 @@ const Swap: FC<Props> = ({
               </Box>
             </FormField>
           </Box>
-          <Box width="434px" direction="column" gap="small">
+          <Box width="470px" direction="column" gap="small">
             <ApproveToken
               symbol={tokenSendedSymbol}
               spender={addresses[protocolName].swap}
