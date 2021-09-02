979c3661944edbdb90892f0aed4736513819f55b
Mark Vujevits <vujevits.mark@gmail.com>
2021-09-02 15:00:08 +0200

    change wording, only show when on the wrong network

--- packages/app/src/components/testnet-banner/index.tsx
+++ packages/app/src/components/testnet-banner/index.tsx
@@ -1,11 +1,20 @@
+import { ChainId, useEthers } from "@usedapp/core";
 import { Box, Text } from "grommet";
 import { FC } from "react";
 
 const TestnetBanner: FC = () => {
+  const { chainId } = useEthers();
+
+  if (chainId === ChainId.Rinkeby) {
+    return null;
+  }
+
+  const message = chainId === undefined ? "Please connect your Wallet" : "Please switch to Rinkeby to use Tenderize.me";
+
   return (
     <Box background="#FFE8B9" height="40px" align="center" justify="around">
       <Text color="gray" size="small">
-        Tenderize.me is currently only live on Rinkeby testnet.
+        {message}
       </Text>
     </Box>
   );
