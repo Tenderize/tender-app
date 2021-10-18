3e29d9a3d1782d9db83ae2b49978b5a63f4a923c
Mark Vujevits <vujevits.mark@gmail.com>
2021-10-18 16:01:21 +0200

    initial hero version

--- packages/app/src/components/highlights/Intro.tsx
+++ packages/app/src/components/highlights/Intro.tsx
@@ -1,7 +1,8 @@
 import { FC } from "react";
 import { Box, Heading, Paragraph } from "grommet";
+import Link from "next/link";
+import { XLButton } from "../base";
 
-// 3496 x 2318
 export const Intro: FC = () => {
   return (
     <Box
@@ -23,14 +24,37 @@ export const Intro: FC = () => {
           style={{
             width: "100vw",
             aspectRatio: "3496/2318",
-            backgroundImage: "url('/redesign/01_rewards.svg')",
+            backgroundImage: "url('/redesign/00_hero.svg')",
             backgroundRepeat: "no-repeat",
             backgroundSize: "contain",
           }}
         >
-          <Box style={{ position: "relative", marginLeft: "15%", marginTop: "18%" /* left: "52%", top: "25%" */ }}>
+          <Box style={{ position: "relative", marginLeft: "52%", marginTop: "18%" }}>
             <Heading>Tenderize</Heading>
             <Paragraph margin="none">The tastiest liquid stake farming money can buy.</Paragraph>
+            <Box direction="row" gap="medium" pad={{ top: "large" }}>
+              <Link href="/stakers/livepeer">
+                <XLButton
+                  secondary
+                  size="large"
+                  label="Open App"
+                  style={{ color: "white", borderColor: "white", borderRadius: 4 }}
+                />
+              </Link>
+              <Link href="/stakers/livepeer">
+                <XLButton
+                  secondary
+                  border="0"
+                  size="large"
+                  label="Learn more"
+                  style={{
+                    color: "white",
+                    border: "none",
+                    justifyContent: "center",
+                  }}
+                />
+              </Link>
+            </Box>
           </Box>
         </Box>
       </Box>
