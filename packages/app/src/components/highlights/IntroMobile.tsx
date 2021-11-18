8d8b6c8f3f4683bb2b1a0fec592efbe84c976e45
Mark Vujevits <vujevits.mark@gmail.com>
2021-11-18 17:17:29 +0100

    social icons on mobile

--- packages/app/src/components/highlights/IntroMobile.tsx
+++ packages/app/src/components/highlights/IntroMobile.tsx
@@ -1,47 +1,79 @@
-import { FC } from "react";
-import { Heading, Paragraph } from "grommet";
+import React, { FC } from "react";
+import { Anchor, Heading, Paragraph } from "grommet";
+import { faDiscord, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const IntroMobile: FC = () => {
   return (
-    <div
-      style={{
-        scrollSnapAlign: "start",
-        flexShrink: 0,
-        width: "100vw",
-        height: "100vh",
-        backgroundImage: `url("/landing/shad-intro.jpg"), url('/landing/noise.png')`,
-        backgroundBlendMode: "darken",
-        backgroundRepeat: "no-repeat, repeat",
-        backgroundSize: "contain, 50px 50px",
-        backgroundPosition: "center",
-        position: "relative",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <>
+      <div style={{ alignSelf: "flex-end", paddingTop: "1rem" }}>
+        <Anchor
+          size="large"
+          color="white"
+          a11yTitle="Chat with us on Discord"
+          href="https://discord.gg/WXR5VBttP5"
+          icon={<FontAwesomeIcon icon={faDiscord} />}
+          target="_blank"
+          style={{ paddingLeft: 0 }}
+        />
+        <Anchor
+          size="large"
+          color="white"
+          a11yTitle="Follow us on Twitter"
+          href="https://twitter.com/tenderize_me"
+          icon={<FontAwesomeIcon icon={faTwitter} />}
+          target="_blank"
+        />
+      </div>
       <div
         style={{
-          height: "50vh",
+          scrollSnapAlign: "start",
+          flexShrink: 0,
+          width: "100vw",
+          height: "100vh",
+          backgroundImage: `url("/landing/shad-intro.jpg"), url('/landing/noise.png')`,
+          backgroundBlendMode: "darken",
+          backgroundRepeat: "no-repeat, repeat",
+          backgroundSize: "contain, 50px 50px",
+          backgroundPosition: "center",
           position: "relative",
-          justifyContent: "flex-start",
+          display: "flex",
+          flexDirection: "column",
           alignItems: "center",
         }}
       >
-        <img
+        <div
           style={{
-            height: "400%",
-            transformOrigin: "top",
-            transform: "scale(0.25)",
+            height: "50vh",
+            position: "relative",
+            justifyContent: "flex-start",
+            alignItems: "center",
           }}
-          src={`/landing/mobile-intro.svg`}
-        />
+        >
+          <img
+            style={{
+              height: "400%",
+              transformOrigin: "top",
+              transform: "scale(0.25)",
+            }}
+            src={`/landing/mobile-intro.svg`}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Heading margin="small" style={{ textShadow: "0px 0px 17px #AD01FF" }}>
+            Tenderize
+          </Heading>
+          <Paragraph margin="small">The tastiest</Paragraph>
+          <Paragraph margin="none"> liquid staking farming</Paragraph>
+          <Paragraph margin="small"> money can buy.</Paragraph>
+        </div>
       </div>
-      <Heading style={{ textShadow: "0px 0px 17px #AD01FF" }}>Tenderize</Heading>
-      <Paragraph margin="small">The tastiest</Paragraph>
-      <Paragraph margin="small"> liquid staking farming</Paragraph>
-      <Paragraph margin="small"> money can buy.</Paragraph>
-    </div>
+    </>
   );
 };
