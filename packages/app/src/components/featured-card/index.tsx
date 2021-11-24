d1bca0946e6374de68e0bdfcbc562cd125f75622
Nico Vergauwen <vergauwennico@gmail.com>
2021-11-24 01:10:23 +0100

    remove static GRT APY

--- packages/app/src/components/featured-card/index.tsx
+++ packages/app/src/components/featured-card/index.tsx
@@ -35,8 +35,7 @@ const FeaturedCards: FC = () => {
           apyInPoints = Math.pow(1 + yearlyAvarageRate / 365, 365) - 1;
         } 
     }
-    let apy = (apyInPoints * 100).toFixed(2);
-    if (key === "/stakers/graph") apy = "39.74"
+    const apy = (apyInPoints * 100).toFixed(2);
     cards.push(<TokenCard key={key} info={stakers[key]} url={key} apy={apy} />);
   }
   return (
