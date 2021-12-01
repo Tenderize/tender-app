5eb43871647522d29bed57db1c2626abfbb414f5
Mark Vujevits <vujevits.mark@gmail.com>
2021-12-01 18:14:28 +0100

    minor cleanup

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -1,22 +1,8 @@
-import { FC, lazy, Suspense } from "react";
-import { GrommetWrapper } from "../components/GrommetWrapper";
-import { useIsTouchDevice } from "../utils/useIsTouchDevice";
-
-const MobileComponent = lazy(() => import("./MobileLandingContainer"));
-const DesktopComponent = lazy(() => import("./DesktopLandingContainer"));
+import { FC } from "react";
+import Token from "./stakers/[slug]";
 
 const Home: FC = () => {
-  const isTouchDevice = useIsTouchDevice();
-
-  return (
-    <GrommetWrapper
-      style={{
-        background: "#0B0C0F",
-      }}
-    >
-      <Suspense fallback={null}>{isTouchDevice ? <MobileComponent /> : <DesktopComponent />}</Suspense>
-    </GrommetWrapper>
-  );
+  return <Token />;
 };
 
 export default Home;
