a187976c7f7b4038ac4525a07ebafa42e6e1519e
Mark Vujevits <vujevits.mark@gmail.com>
2021-12-02 17:58:21 +0100

    break up defi copy two 2 paragraphs

--- packages/landing/src/components/highlights/ConnectWithDeFi.tsx
+++ packages/landing/src/components/highlights/ConnectWithDeFi.tsx
@@ -11,12 +11,15 @@ export const ConnectWithDeFi: FC<{ screenSize: ScreenSize; setVisibleIndex: (v: 
 }) => {
   return (
     <HighlightContainer item="defi" setVisibleIndex={setVisibleIndex} index={index}>
-      <Box style={{ position: "relative", marginLeft: "52%", marginTop: "16%" }}>
+      <Box style={{ position: "relative", marginLeft: "50%", marginTop: "16%" }}>
         <Heading style={{ textShadow: "0px 0px 8px #0075FF" }} size={screenToFontSize(screenSize)}>
           Connect with DeFi
         </Heading>
         <Paragraph margin="none" size={screenToFontSize(screenSize)}>
-          {Copy.DEFI}
+          {Copy.DEFI_1}
+        </Paragraph>
+        <Paragraph margin={{ top: "small" }} size={screenToFontSize(screenSize)}>
+          {Copy.DEFI_2}
         </Paragraph>
       </Box>
     </HighlightContainer>
