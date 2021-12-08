55c86e0da193c52d3a555e017077ea04724f7196
Reuben Rodrigues <63185771+reubenr0d@users.noreply.github.com>
2021-12-09 00:00:49 +0530

    Fix typo (#147)

--- packages/landing/src/components/highlights/constants.ts
+++ packages/landing/src/components/highlights/constants.ts
@@ -5,6 +5,6 @@ export enum Copy {
   STAKING_2 = "Simply deposit your tokens to earn rewards and watch your balance increase, delegations are managed by the Tenderize protocol.",
   REWARDS_1 = "Tenderize automatically compounds rewards on a regular basis.",
   REWARDS_2 = "Stake delegations to nodes are managed according to their profitability, reliability and performance.",
-  DEFI_1 = "TenderTokens unleash the composability between DeFi and staked assest, allowing you to earn yield on top of yield.",
+  DEFI_1 = "TenderTokens unleash the composability between DeFi and staked assets, allowing you to earn yield on top of yield.",
   DEFI_2 = "TenderTokens provide a new financial primitive that unlocks a wide array of new use cases for your staked web3 tokens.",
 }
