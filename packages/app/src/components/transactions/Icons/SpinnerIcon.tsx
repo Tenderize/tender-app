ffb0d1db5ffbc89e659c5b7e90a5ba6e8fb785af
Mark Vujevits <vujevits.mark@gmail.com>
2021-12-10 15:20:22 +0100

    prettify all and fix commands in package.json

--- packages/app/src/components/transactions/Icons/SpinnerIcon.tsx
+++ packages/app/src/components/transactions/Icons/SpinnerIcon.tsx
@@ -3,23 +3,23 @@ import styled from "styled-components";
 
 type props = {
   fill: string;
-}
+};
 
-
-export const SpinnerIcon: FC<props> = ({fill}) => {
+export const SpinnerIcon: FC<props> = ({ fill }) => {
   const Circle = styled.circle`
-  fill: transparent;
-  stroke: ${fill};
-  stroke-linecap: round;
-  stroke-dasharray: 280;
-  stroke-dashoffset: 100;
-  stroke-width: 8px;
-`;
-return (  <Svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-<Circle cx="50" cy="50" r="40" />
-  </Svg>)
-}
-
+    fill: transparent;
+    stroke: ${fill};
+    stroke-linecap: round;
+    stroke-dasharray: 280;
+    stroke-dashoffset: 100;
+    stroke-width: 8px;
+  `;
+  return (
+    <Svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <Circle cx="50" cy="50" r="40" />
+    </Svg>
+  );
+};
 
 const Svg = styled.svg`
   animation: 1s linear infinite svg-animation;
@@ -33,5 +33,3 @@ const Svg = styled.svg`
     }
   }
 `;
-
-
