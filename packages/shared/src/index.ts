f0c88b21a724a96a6a81126094c12f795eefefd6
Mark Vujevits <vujevits.mark@gmail.com>
2022-01-17 16:14:26 +0100

    remove unused import

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,5 +3,5 @@ export { InfoCard } from "./components/tenderizers/infocard";
 export { GrommetWrapper } from "./components/GrommetWrapper";
 export { useIsTouchDevice } from "./hooks/useIsTouchDevice";
 export { theme } from "./theme";
-export { Config, apolloClient } from "./config";
+export { apolloClient } from "./config";
 export * as Queries from "./queries";
