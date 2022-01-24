374f0ed4c7138c7dff39ca7bc9d46c6565d7ca18
Nico Vergauwen <vergauwennico@gmail.com>
2022-01-24 14:24:49 +0100

    add proper notification for faucet

--- packages/app/src/components/faucet/index.tsx
+++ packages/app/src/components/faucet/index.tsx
@@ -3,33 +3,46 @@ import { contracts } from "@tender/contracts";
 import { Button } from "grommet";
 import { useContractFunction } from "@usedapp/core";
 import Dialog from "components/swap/Dialog";
-
+import { LoadingButtonContent } from "components/LoadingButtonContent";
+import {isPendingTransaction} from "../../utils/transactions"
 type props = {
   symbol: string;
   name: string;
 };
 
 const Faucet: FC<props> = ({ symbol, name }) => {
-  const { send } = useContractFunction(contracts[name].faucet, "request");
+
+  const { state: requestTx, send: request} = useContractFunction(contracts[name].faucet, "request", {
+    transactionName: `Requesting ${symbol} from faucet`,
+  });
 
   // TODO why is this needed
   const requestTokens = () => {
-    send();
+    request();
   };
 
   return (
     <Dialog
+      width="large"
       openButtonLabel="Faucet"
       title="Faucet"
       description={`Get some testnet ${symbol} and ETH (you need ETH to get ${symbol})`}
-      button1={<Button primary onClick={requestTokens} label={`Get ${symbol}`} />}
+      button1={
+        <Button
+          primary 
+          style={{width: "250px"}}
+          onClick={requestTokens} 
+          label={isPendingTransaction(requestTx) ? <LoadingButtonContent label={`Request ${symbol}`} /> : `Get ${symbol}`}
+          disabled={isPendingTransaction(requestTx)}
+        />
+      }
       button2={
         <Button
           primary
+          style={{width: "250px"}}
           href="https://www.rinkebyfaucet.com/"
           target="_blank"
           label="Get ETH"
-          style={{ textAlign: "center" }}
         />
       }
     />
