2857faa571c557e93b3b9a7690fb3fbb8eb95bf5
Nico Vergauwen <vergauwennico@gmail.com>
2022-01-24 14:55:06 +0100

    app: add PendingSignature to isPendingTransaction helper

--- packages/app/src/utils/transactions.ts
+++ packages/app/src/utils/transactions.ts
@@ -3,6 +3,7 @@ import { TransactionStatus } from "@usedapp/core"
 export const isPendingTransaction = (tx: TransactionStatus) => {
     switch(tx.status){
         case "Mining": return true
+        case "PendingSignature": return true
         default: return false
     }
 }
