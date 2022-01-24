8c85303ab54efb3a3d5a807fcb65e5def434495e
Nico Vergauwen <vergauwennico@gmail.com>
2022-01-24 14:25:09 +0100

    app: add transaction status helpers ot utils

---
+++ packages/app/src/utils/transactions.ts
@@ -0,0 +1,15 @@
+import { TransactionStatus } from "@usedapp/core"
+
+export const isPendingTransaction = (tx: TransactionStatus) => {
+    switch(tx.status){
+        case "Mining": return true
+        default: return false
+    }
+}
+
+export const isSuccesfulTransaction = (tx: TransactionStatus) => {
+    switch(tx.status) {
+        case "Success": return true
+        default: return false
+    }
+}
