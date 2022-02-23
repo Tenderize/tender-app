0792b2819a72cfbdf1a4bb37bf9ed15f33f1c4e9
Mark Vujevits <vujevits.mark@gmail.com>
2022-02-23 16:18:44 +0100

    improve faucet (#221)
    * improve faucet
    
    * Update packages/app/src/components/faucet/index.tsx
    
    Co-authored-by: Nico Vergauwen <vergauwennico@gmail.com>
    
    Co-authored-by: Nico Vergauwen <vergauwennico@gmail.com>

--- packages/app/src/components/faucet/index.tsx
+++ packages/app/src/components/faucet/index.tsx
@@ -1,24 +1,29 @@
 import { FC } from "react";
 import { contracts } from "@tender/contracts";
 import { Button } from "grommet";
-import { useContractFunction } from "@usedapp/core";
+import { ChainId, useContractFunction, useEtherBalance, useEthers } from "@usedapp/core";
 import Dialog from "components/swap/Dialog";
 import { LoadingButtonContent } from "components/LoadingButtonContent";
 import { isPendingTransaction } from "../../utils/transactions";
-type props = {
+import stakers from "data/stakers";
+import { constants } from "ethers";
+
+const Faucet: FC<{
   symbol: string;
   protocolName: string;
-};
-
-const Faucet: FC<props> = ({ symbol, protocolName }) => {
+}> = ({ symbol, protocolName }) => {
   const { state: requestTx, send: request } = useContractFunction(contracts[protocolName].faucet, "request", {
     transactionName: `Requesting ${symbol} from faucet`,
   });
+
+  const { account } = useEthers();
+  const balance = useEtherBalance(account);
 
   // TODO why is this needed
   const requestTokens = () => {
     request();
   };
+  const isRinkebyArbitrum = stakers[protocolName].chainId === ChainId.ArbitrumRinkeby;
 
   return (
     <Dialog
@@ -26,8 +31,26 @@ const Faucet: FC<props> = ({ symbol, protocolName }) => {
       width="large"
       openButtonLabel="Faucet"
       title="Faucet"
-      description={`Get some testnet ${symbol} and ETH (you need ETH to get ${symbol})`}
-      button1={
+      description={`Get some testnet ETH first, then some ${symbol}.
+${isRinkebyArbitrum ? "To get ETH on ArbitrumRinkeby, you have to bridge it from Rinkeby." : ""}
+          `}
+      buttons={[
+        <Button
+          primary
+          style={{ width: "100%" }}
+          href="https://www.rinkebyfaucet.com/"
+          target="_blank"
+          label="Get ETH"
+        />,
+        isRinkebyArbitrum && (
+          <Button
+            primary
+            style={{ width: "100%" }}
+            href="https://bridge.arbitrum.io/"
+            target="_blank"
+            label="Bridge ETH"
+          />
+        ),
         <Button
           primary
           style={{ width: "100%" }}
@@ -35,18 +58,9 @@ const Faucet: FC<props> = ({ symbol, protocolName }) => {
           label={
             isPendingTransaction(requestTx) ? <LoadingButtonContent label={`Request ${symbol}`} /> : `Get ${symbol}`
           }
-          disabled={isPendingTransaction(requestTx)}
-        />
-      }
-      button2={
-        <Button
-          primary
-          style={{ width: "100%" }}
-          href="https://www.rinkebyfaucet.com/"
-          target="_blank"
-          label="Get ETH"
-        />
-      }
+          disabled={balance?.eq(constants.Zero) || isPendingTransaction(requestTx)}
+        />,
+      ]}
     />
   );
 };
