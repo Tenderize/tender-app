f70760d70bf176f6bc497747669df006860638fc
Mark Vujevits <vujevits.mark@gmail.com>
2022-05-05 10:39:33 +0200

    use ghost for blog content on landing

--- packages/landing/src/pages/api/medium.ts
+++ packages/landing/src/pages/api/medium.ts
@@ -1,7 +1,7 @@
 import { NextApiResponse, NextApiRequest } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const data = await fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@tenderize");
+  const data = await fetch("https://api.rss2json.com/v1/api.json?rss_url=https://blog.tenderize.me/rss/");
   const jsonObj = await data.json();
   res.status(200).json(jsonObj);
 };
