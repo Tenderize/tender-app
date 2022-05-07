b248520517d0be2bfc08fd5273b8125ae95c714c
Reuben Rodrigues <reubenrodrigues24@gmail.com>
2022-05-07 16:12:33 +0700

    Subgraph endpoints

--- packages/shared/src/graph.ts
+++ packages/shared/src/graph.ts
@@ -5,6 +5,8 @@ import { MultiAPILink } from "@habx/apollo-multi-endpoint-link";
 const ENDPOINTS: Endpoints = {
   [ChainId.Rinkeby]: "https://api.thegraph.com/subgraphs/name/tenderize/tenderize-staging",
   [ChainId.ArbitrumRinkeby]: "https://api.thegraph.com/subgraphs/name/tenderize/tenderize-arbitrum-staging",
+  [ChainId.Arbitrum]: "https://api.thegraph.com/subgraphs/name/tenderize/tenderize-arbitrum",
+  [ChainId.Mainnet]: "https://thegraph.com/explorer/subgraph/tenderize/tenderize-ethereum",
 };
 
 interface Endpoints {
