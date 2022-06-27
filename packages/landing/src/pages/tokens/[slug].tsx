aacd5723651e0ba63f974d730cb55648f1db665c
Mark <vujevits.mark@gmail.com>
2022-06-27 10:42:52 +0200

    ticker info for coingecko integration (#296)
    * sidebar and routing for tickerinfo page
    
    * dump tickerinfo data to view
    
    * table with token-info added
    
    Co-authored-by: Mark <vmaark@users.noreply.github.com>
    
    * add all details and some styles
    
    * Token page: default to grt
    
    * Tokens page: fix links
    
    Co-authored-by: Reuben Rodrigues <reubenrodrigues24@gmail.com>
    Co-authored-by: Mark <vmaark@users.noreply.github.com>

---
+++ packages/landing/src/pages/tokens/[slug].tsx
@@ -0,0 +1,146 @@
+import { FC, useEffect, useState } from "react";
+import { Box, Button, ButtonExtendedProps, Nav, Table, TableBody, TableCell, TableRow, Text } from "grommet";
+import { Foot, GrommetWrapper, stakers } from "@tender/shared/src/index";
+import Navbar from "../../components/nav";
+import { tickerInfo, TickerInfo } from "data/tickerInfo";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const stakersArray = Object.values(stakers);
+
+const TickerInfoPage: FC = () => {
+  const router = useRouter();
+  const [activeToken, setActiveToken] = useState(router.query.slug as string);
+
+  useEffect(() => {
+    setActiveToken(router.query.slug as string);
+  }, [router.query.slug]);
+
+  const currentTickerInfo = tickerInfo.find((item) => item.symbol.toLowerCase() === activeToken) as TickerInfo;
+  return (
+    <GrommetWrapper
+      style={{
+        background: "#0B0C0F",
+      }}
+    >
+      <Navbar />
+      <Box direction="row">
+        <Nav width="medium" gap="small">
+          {tickerInfo.map((ticker) => {
+            const staker = stakersArray.find((staker) => ticker.symbol.includes(staker.symbol));
+            const logo = (ticker.derivative ? staker?.bwTenderLogo : staker?.bwLogo) ?? "";
+            return (
+              <SidebarButton
+                logo={`/${logo}`}
+                label={ticker.symbol}
+                active={ticker.symbol.toLowerCase() === activeToken}
+                key={ticker.symbol}
+                justify="between"
+              />
+            );
+          })}
+        </Nav>
+        <Box>
+          <Table margin="xlarge">
+            <TableBody>
+              <TableRow>
+                <TableCell scope="row" verticalAlign="top" size="small">
+                  <strong>Name</strong>
+                </TableCell>
+                <TableCell>{currentTickerInfo.name}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell scope="row" verticalAlign="top">
+                  <strong>Symbol</strong>
+                </TableCell>
+                <TableCell>{currentTickerInfo.symbol}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell scope="row" verticalAlign="top">
+                  <strong>Whitepaper</strong>
+                </TableCell>
+                <TableCell>
+                  <a href={currentTickerInfo.whitepaper} target="_blank">
+                    {currentTickerInfo.whitepaper}
+                  </a>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell scope="row" verticalAlign="top">
+                  <strong>Website</strong>
+                </TableCell>
+                <TableCell>
+                  <a href={currentTickerInfo.website} target="_blank">
+                    {currentTickerInfo.website}
+                  </a>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell scope="row" verticalAlign="top">
+                  <strong>Explorer Link</strong>
+                </TableCell>
+                <TableCell>
+                  <a href={currentTickerInfo.explorer_link} target="_blank">
+                    {currentTickerInfo.explorer_link}
+                  </a>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell scope="row" verticalAlign="top">
+                  <strong>Description</strong>
+                </TableCell>
+                <TableCell>{currentTickerInfo.description}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Box>
+      </Box>
+      <div
+        style={{
+          position: "absolute",
+          width: "100%",
+          bottom: 0,
+        }}
+      >
+        <Foot />
+      </div>
+    </GrommetWrapper>
+  );
+};
+
+const SidebarButton: FC<ButtonExtendedProps & { logo: string; label: string }> = (props) => {
+  const { label, logo, active, ...rest } = props;
+  return (
+    <Button plain {...rest}>
+      {({ hover }) => (
+        <Link href={`/tokens/${label.toLowerCase()}`} passHref>
+          <Box
+            background={hover ? "brand" : active ? "darkgrey" : "#0B0C0F"}
+            pad={{ horizontal: "large", vertical: "medium" }}
+            justify="between"
+            direction="row"
+          >
+            <Image width={30} height={30} src={logo} />
+            <Text size="medium">{label}</Text>
+          </Box>
+        </Link>
+      )}
+    </Button>
+  );
+};
+
+export const getStaticProps = async () => {
+  return {
+    props: {},
+  };
+};
+
+export const getStaticPaths = async () => {
+  return {
+    paths: tickerInfo.map((ticker) => ({ params: { slug: ticker.symbol.toLowerCase() } })),
+    fallback: false,
+  };
+};
+
+export default TickerInfoPage;
