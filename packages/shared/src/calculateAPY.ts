716747d8699dcc32a3c4aefdf8589f4a222e798e
kyriediculous <vergauwennico@gmail.com>
2022-10-07 22:49:20 +0200

    pretty

--- packages/shared/src/calculateAPY.ts
+++ packages/shared/src/calculateAPY.ts
@@ -2,7 +2,7 @@ import { Queries, stakers, Staker } from "@tender/shared/src/index";
 import { ProtocolName } from "./data/stakers";
 
 const YEAR_IN_SECONDS = 60 * 60 * 24 * 365;
-type apycalc = { apy: number, timeDiff: number }
+type apycalc = { apy: number; timeDiff: number };
 
 export const calculateAPY = (data: Queries.TenderizerDaysType | undefined): Record<Staker["name"], Staker> => {
   const stakersWithAPY = Object.values(stakers).map((staker) => {
@@ -13,28 +13,27 @@ export const calculateAPY = (data: Queries.TenderizerDaysType | undefined): Reco
       rewardsClaimedEvents: [],
     };
     const rewardsClaimedEvents = tenderizerData.rewardsClaimedEvents.filter((item) => item.rewards !== "0");
-    let totalTimePassed = 0
-    const apysBasedOnSingleEvents: Array<apycalc> = rewardsClaimedEvents
-      .map((value, index) => {
-        console.log(value)
-        // we need the first value's timestamp but not the rewards
-        if (index === 0) {
-          return { apy: 0, timeDiff: 0 };
-        }
+    let totalTimePassed = 0;
+    const apysBasedOnSingleEvents: Array<apycalc> = rewardsClaimedEvents.map((value, index) => {
+      console.log(value);
+      // we need the first value's timestamp but not the rewards
+      if (index === 0) {
+        return { apy: 0, timeDiff: 0 };
+      }
 
-        const currentEvent = value;
-        const previousEvent = rewardsClaimedEvents[index - 1];
+      const currentEvent = value;
+      const previousEvent = rewardsClaimedEvents[index - 1];
 
-        const rate = Number.parseFloat(currentEvent.rewards) / Number.parseFloat(currentEvent.oldPrincipal);
-        const timeDiff = currentEvent.timestamp - previousEvent.timestamp;
-        totalTimePassed += timeDiff
-        const compoundsPerYear = Math.floor(YEAR_IN_SECONDS / timeDiff);
-        const apy = Math.pow(1 + rate, compoundsPerYear) - 1;
-        return { apy: apy || 0, timeDiff: timeDiff || 0 };
-      })
+      const rate = Number.parseFloat(currentEvent.rewards) / Number.parseFloat(currentEvent.oldPrincipal);
+      const timeDiff = currentEvent.timestamp - previousEvent.timestamp;
+      totalTimePassed += timeDiff;
+      const compoundsPerYear = Math.floor(YEAR_IN_SECONDS / timeDiff);
+      const apy = Math.pow(1 + rate, compoundsPerYear) - 1;
+      return { apy: apy || 0, timeDiff: timeDiff || 0 };
+    });
 
-    const normalizeApys = apysBasedOnSingleEvents.slice(1).map(o => o.apy * o.timeDiff)
-    const final = normalizeApys.reduce((p, n) => p + n) / totalTimePassed
+    const normalizeApys = apysBasedOnSingleEvents.slice(1).map((o) => o.apy * o.timeDiff);
+    const final = normalizeApys.reduce((p, n) => p + n) / totalTimePassed;
 
     const apy = final ? (final * 100).toFixed(2) : "0";
     return {
