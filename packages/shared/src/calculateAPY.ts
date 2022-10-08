796823e2d60b790bbc234c439b9d356a6e041ac1
Nico Vergauwen <vergauwennico@gmail.com>
2022-10-08 04:15:09 +0200

    Time-weights for reward periods when calculating APY (#329)
    * time-weight for reward periods
    
    * type fix
    
    * lint
    
    * pretty

--- packages/shared/src/calculateAPY.ts
+++ packages/shared/src/calculateAPY.ts
@@ -2,42 +2,40 @@ import { Queries, stakers, Staker } from "@tender/shared/src/index";
 import { ProtocolName } from "./data/stakers";
 
 const YEAR_IN_SECONDS = 60 * 60 * 24 * 365;
+type apycalc = { apy: number; timeDiff: number };
 
 export const calculateAPY = (data: Queries.TenderizerDaysType | undefined): Record<Staker["name"], Staker> => {
   const stakersWithAPY = Object.values(stakers).map((staker) => {
-    let apyInPoints = 0;
-    if (data != null) {
-      const tenderizerData = data.tenderizers.find((item) => item.id.includes(staker.subgraphId)) ?? {
-        id: staker.subgraphId,
-        rewardsClaimedEvents: [],
-      };
-      const rewardsClaimedEvents = tenderizerData.rewardsClaimedEvents.filter((item) => item.rewards !== "0");
+    if (data == null) return 0;
 
-      if (rewardsClaimedEvents.length === 0) {
-        apyInPoints = 0;
-      } else {
-        const apysBasedOnSingleEvents = rewardsClaimedEvents
-
-          .map((value, index) => {
-            // we need the first value's timestamp but not the rewards
-            if (index === 0) {
-              return null;
-            }
-
-            const currentEvent = value;
-            const previousEvent = rewardsClaimedEvents[index - 1];
-            const rate = Number.parseFloat(currentEvent.rewards) / Number.parseFloat(currentEvent.oldPrincipal);
-            const timeDiff = currentEvent.timestamp - previousEvent.timestamp;
-            const compoundsPerYear = Math.floor(YEAR_IN_SECONDS / timeDiff);
-            const apy = Math.pow(1 + rate, compoundsPerYear) - 1;
-            return apy;
-          })
-          .filter((item): item is number => item != null);
-        apyInPoints =
-          apysBasedOnSingleEvents.reduce((prev, current) => prev + current, 0) / apysBasedOnSingleEvents.length;
+    const tenderizerData = data.tenderizers.find((item) => item.id.includes(staker.subgraphId)) ?? {
+      id: staker.subgraphId,
+      rewardsClaimedEvents: [],
+    };
+    const rewardsClaimedEvents = tenderizerData.rewardsClaimedEvents.filter((item) => item.rewards !== "0");
+    let totalTimePassed = 0;
+    const apysBasedOnSingleEvents: Array<apycalc> = rewardsClaimedEvents.map((value, index) => {
+      console.log(value);
+      // we need the first value's timestamp but not the rewards
+      if (index === 0) {
+        return { apy: 0, timeDiff: 0 };
       }
-    }
-    const apy = (apyInPoints * 100).toFixed(2);
+
+      const currentEvent = value;
+      const previousEvent = rewardsClaimedEvents[index - 1];
+
+      const rate = Number.parseFloat(currentEvent.rewards) / Number.parseFloat(currentEvent.oldPrincipal);
+      const timeDiff = currentEvent.timestamp - previousEvent.timestamp;
+      totalTimePassed += timeDiff;
+      const compoundsPerYear = Math.floor(YEAR_IN_SECONDS / timeDiff);
+      const apy = Math.pow(1 + rate, compoundsPerYear) - 1;
+      return { apy: apy || 0, timeDiff: timeDiff || 0 };
+    });
+
+    const normalizeApys = apysBasedOnSingleEvents.slice(1).map((o) => o.apy * o.timeDiff);
+    const final = normalizeApys.reduce((p, n) => p + n) / totalTimePassed;
+
+    const apy = final ? (final * 100).toFixed(2) : "0";
     return {
       ...staker,
       apy,
@@ -46,6 +44,7 @@ export const calculateAPY = (data: Queries.TenderizerDaysType | undefined): Reco
 
   const stakersWithAPYMap = {} as Record<ProtocolName, Staker>;
   for (const staker of stakersWithAPY) {
+    if (staker === 0) continue;
     stakersWithAPYMap[staker.name] = staker;
   }
   return stakersWithAPYMap;
