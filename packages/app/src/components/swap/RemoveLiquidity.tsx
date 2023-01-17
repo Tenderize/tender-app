ef814d8e5bc6e1c3135f647b7c561fde007ca03c
Mark Vujevits <vujevits.mark@gmail.com>
2023-01-17 20:55:46 +0100

    refactor removeLiquidity

--- packages/app/src/components/swap/RemoveLiquidity.tsx
+++ packages/app/src/components/swap/RemoveLiquidity.tsx
@@ -37,7 +37,7 @@ import {
 import { FormClose } from "grommet-icons";
 import { isPendingTransaction } from "utils/transactions";
 import { useResetInputAfterTx } from "utils/useResetInputAfterTx";
-import { ProtocolName } from "@tender/shared/src/data/stakers";
+import { ProtocolName, Staker } from "@tender/shared/src/data/stakers";
 
 type Props = {
   protocolName: ProtocolName;
@@ -48,92 +48,10 @@ type Props = {
 const RemoveLiquidity: FC<Props> = ({ protocolName, symbol, lpTokenBalance }) => {
   const staker = stakers[protocolName];
   const [show, setShow] = useState(false);
-  const [tabIndex, setTabIndex] = useState(0);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const [isMulti, setIsMulti] = useState(true);
-  const [lpSharesInputMulti, setLpSharesInputMulti] = useState("");
-  const [lpSharesInputSingle, setLpSharesInputSingle] = useState("");
-
-  const [tokenOutput, setTokenOutput] = useState("");
-  const [tenderOutput, setTenderOutput] = useState("");
   const [selectedToken, setSelectedToken] = useState(symbol);
-  const { account } = useEthers();
-
-  const isLpSharesApproved = useIsTokenApproved(
-    addresses[protocolName].lpToken,
-    account,
-    addresses[protocolName].tenderSwap,
-    lpSharesInputMulti || lpSharesInputSingle
-  );
-
-  const { removeLiquiditySingleOut, tx: exitPoolSingleTx } = useExitPoolSingle(
-    addresses[protocolName].lpToken,
-    protocolName,
-    account,
-    addresses[protocolName].tenderSwap,
-    symbol,
-    isLpSharesApproved
-  );
-
-  const { removeLiquidity, exitPoolTx } = useExitPool(
-    addresses[protocolName].lpToken,
-    protocolName,
-    account,
-    addresses[protocolName].tenderSwap,
-    symbol,
-    isLpSharesApproved
-  );
-
-  useResetInputAfterTx(exitPoolTx, setLpSharesInputMulti);
-  useResetInputAfterTx(exitPoolSingleTx, setLpSharesInputSingle);
-
-  const singleTokenOutAddress =
-    selectedToken === symbol ? addresses[protocolName].token : addresses[protocolName].tenderToken;
-
-  const singleOut = useCalculateRemoveLiquidityOneToken(
-    addresses[protocolName].tenderSwap,
-    utils.parseEther(lpSharesInputSingle || "0"),
-    singleTokenOutAddress
-  );
-
-  const [tenderOut, tokenOut] = useCalculateRemoveLiquidity(
-    addresses[protocolName].tenderSwap,
-    utils.parseEther(lpSharesInputMulti || "0")
-  );
-
-  useEffect(() => {
-    setTokenOutput(weiToEthWithDecimals(tokenOut, 6));
-    setTenderOutput(weiToEthWithDecimals(tenderOut, 6));
-  }, [tenderOut, tokenOut]);
-
-  const handleRemoveLiquidity = async (e: any) => {
-    e.preventDefault();
-    if (isMulti) {
-      // NOTE: Pool cardinality is tenderToken/Token
-      await removeLiquidity(utils.parseEther(lpSharesInputMulti || "0"), tenderOut, tokenOut);
-    } else {
-      await removeLiquiditySingleOut(utils.parseEther(lpSharesInputSingle || "0"), singleTokenOutAddress, singleOut);
-    }
-  };
-
-  const { priceImpact } = useLiquidityPriceImpact(
-    addresses[protocolName].tenderSwap,
-    false,
-    selectedToken === symbol ? weiToEthWithDecimals(singleOut, 6) : "0",
-    selectedToken === symbol ? "0" : weiToEthWithDecimals(singleOut, 6)
-  );
-
-  const onActive = useCallback((nextIndex: number) => {
-    setTabIndex(nextIndex);
-    if (nextIndex === 0) {
-      setIsMulti(true);
-    } else {
-      setIsMulti(false);
-    }
-  }, []);
 
   const symbolFull = `t${symbol}-SWAP`;
 
@@ -160,7 +78,7 @@ const RemoveLiquidity: FC<Props> = ({ protocolName, symbol, lpTokenBalance }) =>
               </Heading>
             </CardHeader>
             <CardBody>
-              <Tabs id="exit-type" activeIndex={tabIndex} onActive={onActive}>
+              <Tabs id="exit-type">
                 <Tab
                   title={
                     <Box justify="center" align="center">
@@ -168,52 +86,14 @@ const RemoveLiquidity: FC<Props> = ({ protocolName, symbol, lpTokenBalance }) =>
                     </Box>
                   }
                 >
-                  <Box pad={{ top: "medium", horizontal: "large" }} align="center">
-                    <Form style={{ width: "100%" }}>
-                      <Box gap="medium">
-                        <LPTokensToRemoveInputField
-                          lpTokenBalance={lpTokenBalance}
-                          lpSharesInput={lpSharesInputMulti}
-                          setLpSharesInput={setLpSharesInputMulti}
-                          symbolFull={symbolFull}
-                        />
-                        <FormField label="You will receive">
-                          <Box direction="row">
-                            <Box gap="small" fill>
-                              <TextInput
-                                disabled
-                                readOnly
-                                id="exitMultiReceive"
-                                placeholder={`0 ${symbol}`}
-                                icon={
-                                  <Box pad="xsmall" direction="row" align="center" gap="small">
-                                    <Image height="35" src={`/${staker.bwLogo}`} />
-                                    <Text>{symbol}</Text>
-                                  </Box>
-                                }
-                                style={{ textAlign: "right", padding: "20px 50px" }}
-                                value={tokenOutput}
-                              />
-                              <TextInput
-                                readOnly
-                                disabled
-                                id="exitMultiReceive"
-                                placeholder={`0 t${symbol}`}
-                                icon={
-                                  <Box pad="xsmall" direction="row" align="center" gap="small">
-                                    <Image height="35" src={`/${staker.bwTenderLogo}`} />
-                                    <Text>t{symbol}</Text>
-                                  </Box>
-                                }
-                                style={{ textAlign: "right", padding: "20px 50px" }}
-                                value={tenderOutput}
-                              />
-                            </Box>
-                          </Box>
-                        </FormField>
-                      </Box>
-                    </Form>
-                  </Box>
+                  <RemoveMulti
+                    protocolName={protocolName}
+                    symbolFull={symbolFull}
+                    lpTokenBalance={lpTokenBalance}
+                    symbol={symbol}
+                    staker={staker}
+                    setSelectedToken={setSelectedToken}
+                  />
                 </Tab>
                 <Tab
                   title={
@@ -222,85 +102,252 @@ const RemoveLiquidity: FC<Props> = ({ protocolName, symbol, lpTokenBalance }) =>
                     </Box>
                   }
                 >
-                  <Box pad={{ top: "medium", horizontal: "large" }} align="center">
-                    <Form style={{ width: "100%" }}>
-                      <Box gap="medium">
-                        <LPTokensToRemoveInputField
-                          lpTokenBalance={lpTokenBalance}
-                          lpSharesInput={lpSharesInputSingle}
-                          setLpSharesInput={setLpSharesInputSingle}
-                          symbolFull={symbolFull}
-                        />
-                        <Box>
-                          <FormField label="Select Token To Receive" controlId="selectTokenReceive">
-                            <Select
-                              value={
-                                <TextInput
-                                  disabled
-                                  readOnly
-                                  value={utils.formatEther(singleOut)}
-                                  placeholder={"0"}
-                                  type="number"
-                                  style={{ textAlign: "right", padding: "20px 50px", border: "none" }}
-                                  icon={
-                                    <Box pad="xsmall" direction="row" align="center" gap="small">
-                                      <Image
-                                        height="35"
-                                        src={selectedToken === symbol ? `/${staker.bwLogo}` : `/${staker.bwTenderLogo}`}
-                                      />
-                                      <Text>{selectedToken === symbol ? symbol : `t${symbol}`}</Text>
-                                    </Box>
-                                  }
-                                />
-                              }
-                              options={[
-                                <Box direction="row" gap="small" align="center">
-                                  <Image
-                                    height={30}
-                                    width={30}
-                                    src={selectedToken === symbol ? `/${staker.bwTenderLogo}` : `/${staker.bwLogo}`}
-                                    alt="token logo"
-                                  />
-                                  {selectedToken === symbol ? `t${symbol}` : symbol}
-                                </Box>,
-                              ]}
-                              onChange={() => setSelectedToken(selectedToken === symbol ? `t${symbol}` : symbol)}
-                            />
-                          </FormField>
-                          <Text textAlign="end">{`Price impact: ${withDecimals(
-                            (priceImpact * 100).toString(),
-                            2
-                          )} %`}</Text>
-                        </Box>
-                      </Box>
-                    </Form>
-                  </Box>
+                  <RemoveSingle
+                    protocolName={protocolName}
+                    symbolFull={symbolFull}
+                    lpTokenBalance={lpTokenBalance}
+                    selectedToken={selectedToken}
+                    symbol={symbol}
+                    staker={staker}
+                    setSelectedToken={setSelectedToken}
+                  />
                 </Tab>
               </Tabs>
             </CardBody>
-            <CardFooter align="center" justify="center" pad={{ vertical: "medium" }}>
-              <Box style={{ width: "100%" }} pad={{ horizontal: "large" }} justify="center" gap="small">
-                <Button
-                  primary
-                  onClick={handleRemoveLiquidity}
-                  disabled={
-                    !hasValue(lpSharesInputSingle || lpSharesInputMulti) ||
-                    isPendingTransaction(exitPoolSingleTx) ||
-                    isPendingTransaction(exitPoolTx)
-                  }
-                  label={
-                    isPendingTransaction(exitPoolSingleTx) || isPendingTransaction(exitPoolTx) ? (
-                      <LoadingButtonContent label="Removing Liquidity..." />
-                    ) : (
-                      "Remove Liquidity"
-                    )
-                  }
-                />
-              </Box>
-            </CardFooter>
+            <CardFooter align="center" justify="center" pad={{ vertical: "medium" }}></CardFooter>
           </Card>
         </Layer>
       )}
+    </Box>
+  );
+};
+
+const RemoveMulti: FC<{
+  protocolName: ProtocolName;
+  symbolFull: string;
+  lpTokenBalance: BigNumberish;
+  symbol: string;
+  staker: Staker;
+  setSelectedToken: (v: string) => void;
+}> = ({ lpTokenBalance, symbolFull, symbol, staker, protocolName }) => {
+  const { account } = useEthers();
+
+  const [lpSharesInputMulti, setLpSharesInputMulti] = useState("");
+  const [tokenOutput, setTokenOutput] = useState("");
+  const [tenderOutput, setTenderOutput] = useState("");
+
+  const isLpSharesApproved = useIsTokenApproved(
+    addresses[protocolName].lpToken,
+    account,
+    addresses[protocolName].tenderSwap,
+    lpSharesInputMulti
+  );
+  const { removeLiquidity, exitPoolTx } = useExitPool(
+    addresses[protocolName].lpToken,
+    protocolName,
+    account,
+    addresses[protocolName].tenderSwap,
+    symbol,
+    isLpSharesApproved
+  );
+
+  const [tenderOut, tokenOut] = useCalculateRemoveLiquidity(
+    addresses[protocolName].tenderSwap,
+    utils.parseEther(lpSharesInputMulti || "0")
+  );
+
+  useEffect(() => {
+    setTokenOutput(weiToEthWithDecimals(tokenOut, 6));
+    setTenderOutput(weiToEthWithDecimals(tenderOut, 6));
+  }, [tenderOut, tokenOut]);
+
+  useResetInputAfterTx(exitPoolTx, setLpSharesInputMulti);
+
+  const handleRemoveLiquidity = async (e: any) => {
+    e.preventDefault();
+    // NOTE: Pool cardinality is tenderToken/Token
+    await removeLiquidity(utils.parseEther(lpSharesInputMulti || "0"), tenderOut, tokenOut);
+  };
+
+  return (
+    <Box pad={{ top: "medium", horizontal: "large" }} align="center">
+      <Form style={{ width: "100%" }}>
+        <Box gap="medium">
+          <LPTokensToRemoveInputField
+            lpTokenBalance={lpTokenBalance}
+            lpSharesInput={lpSharesInputMulti}
+            setLpSharesInput={setLpSharesInputMulti}
+            symbolFull={symbolFull}
+          />
+          <FormField label="You will receive">
+            <Box direction="row">
+              <Box gap="small" fill>
+                <TextInput
+                  disabled
+                  readOnly
+                  id="exitMultiReceive"
+                  placeholder={`0 ${symbol}`}
+                  icon={
+                    <Box pad="xsmall" direction="row" align="center" gap="small">
+                      <Image height="35" src={`/${staker.bwLogo}`} />
+                      <Text>{symbol}</Text>
+                    </Box>
+                  }
+                  style={{ textAlign: "right", padding: "20px 50px" }}
+                  value={tokenOutput}
+                />
+                <TextInput
+                  readOnly
+                  disabled
+                  id="exitMultiReceive"
+                  placeholder={`0 t${symbol}`}
+                  icon={
+                    <Box pad="xsmall" direction="row" align="center" gap="small">
+                      <Image height="35" src={`/${staker.bwTenderLogo}`} />
+                      <Text>t{symbol}</Text>
+                    </Box>
+                  }
+                  style={{ textAlign: "right", padding: "20px 50px" }}
+                  value={tenderOutput}
+                />
+              </Box>
+            </Box>
+          </FormField>
+        </Box>
+      </Form>
+      <Box style={{ width: "100%" }} pad={{ horizontal: "large" }} justify="center" gap="small">
+        <Button
+          primary
+          onClick={handleRemoveLiquidity}
+          disabled={!hasValue(lpSharesInputMulti) || isPendingTransaction(exitPoolTx)}
+          label={
+            isPendingTransaction(exitPoolTx) ? (
+              <LoadingButtonContent label="Removing Liquidity..." />
+            ) : (
+              "Remove Liquidity"
+            )
+          }
+        />
+      </Box>
+    </Box>
+  );
+};
+
+const RemoveSingle: FC<{
+  protocolName: ProtocolName;
+  symbolFull: string;
+  lpTokenBalance: BigNumberish;
+  selectedToken: string;
+  symbol: string;
+  staker: Staker;
+  setSelectedToken: (v: string) => void;
+}> = ({ lpTokenBalance, symbolFull, protocolName, symbol, selectedToken, staker, setSelectedToken }) => {
+  const { account } = useEthers();
+
+  const [lpSharesInputSingle, setLpSharesInputSingle] = useState("");
+
+  const singleTokenOutAddress =
+    selectedToken === symbol ? addresses[protocolName].token : addresses[protocolName].tenderToken;
+
+  const singleOut = useCalculateRemoveLiquidityOneToken(
+    addresses[protocolName].tenderSwap,
+    utils.parseEther(lpSharesInputSingle || "0"),
+    singleTokenOutAddress
+  );
+
+  const isLpSharesApproved = useIsTokenApproved(
+    addresses[protocolName].lpToken,
+    account,
+    addresses[protocolName].tenderSwap,
+    lpSharesInputSingle
+  );
+
+  const { priceImpact } = useLiquidityPriceImpact(
+    addresses[protocolName].tenderSwap,
+    false,
+    selectedToken === symbol ? weiToEthWithDecimals(singleOut, 6) : "0",
+    selectedToken === symbol ? "0" : weiToEthWithDecimals(singleOut, 6)
+  );
+
+  const { removeLiquiditySingleOut, tx: exitPoolSingleTx } = useExitPoolSingle(
+    addresses[protocolName].lpToken,
+    protocolName,
+    account,
+    addresses[protocolName].tenderSwap,
+    symbol,
+    isLpSharesApproved
+  );
+  useResetInputAfterTx(exitPoolSingleTx, setLpSharesInputSingle);
+
+  const handleRemoveLiquidity = async (e: any) => {
+    e.preventDefault();
+    await removeLiquiditySingleOut(utils.parseEther(lpSharesInputSingle || "0"), singleTokenOutAddress, singleOut);
+  };
+
+  return (
+    <Box pad={{ top: "medium", horizontal: "large" }} align="center">
+      <Form style={{ width: "100%" }}>
+        <Box gap="medium">
+          <LPTokensToRemoveInputField
+            lpTokenBalance={lpTokenBalance}
+            lpSharesInput={lpSharesInputSingle}
+            setLpSharesInput={setLpSharesInputSingle}
+            symbolFull={symbolFull}
+          />
+          <Box>
+            <FormField label="Select Token To Receive" controlId="selectTokenReceive">
+              <Select
+                value={
+                  <TextInput
+                    disabled
+                    readOnly
+                    value={utils.formatEther(singleOut)}
+                    placeholder={"0"}
+                    type="number"
+                    style={{ textAlign: "right", padding: "20px 50px", border: "none" }}
+                    icon={
+                      <Box pad="xsmall" direction="row" align="center" gap="small">
+                        <Image
+                          height="35"
+                          src={selectedToken === symbol ? `/${staker.bwLogo}` : `/${staker.bwTenderLogo}`}
+                        />
+                        <Text>{selectedToken === symbol ? symbol : `t${symbol}`}</Text>
+                      </Box>
+                    }
+                  />
+                }
+                options={[
+                  <Box direction="row" gap="small" align="center">
+                    <Image
+                      height={30}
+                      width={30}
+                      src={selectedToken === symbol ? `/${staker.bwTenderLogo}` : `/${staker.bwLogo}`}
+                      alt="token logo"
+                    />
+                    {selectedToken === symbol ? `t${symbol}` : symbol}
+                  </Box>,
+                ]}
+                onChange={() => setSelectedToken(selectedToken === symbol ? `t${symbol}` : symbol)}
+              />
+            </FormField>
+            <Text textAlign="end">{`Price impact: ${withDecimals((priceImpact * 100).toString(), 2)} %`}</Text>
+          </Box>
+        </Box>
+      </Form>
+      <Box style={{ width: "100%" }} pad={{ horizontal: "large" }} justify="center" gap="small">
+        <Button
+          primary
+          onClick={handleRemoveLiquidity}
+          disabled={!hasValue(lpSharesInputSingle) || isPendingTransaction(exitPoolSingleTx)}
+          label={
+            isPendingTransaction(exitPoolSingleTx) ? (
+              <LoadingButtonContent label="Removing Liquidity..." />
+            ) : (
+              "Remove Liquidity"
+            )
+          }
+        />
+      </Box>
     </Box>
   );
 };
